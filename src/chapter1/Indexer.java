3b79496444cc40ad7d7133d0c53a7cd7db09f4e2
Plamen Milenkov <plamen.milenkov@sap.com>
2016-01-10 19:08:43 +0200

    Chapter 1. Indexer and Searcher

---
+++ src/chapter1/Indexer.java
@@ -0,0 +1,73 @@
+package chapter1;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+public class Indexer {
+	private IndexWriter writer;
+
+	public Indexer(String indexDir) throws IOException {
+		Directory dir = FSDirectory.open(new File(indexDir).toPath());
+		writer = new IndexWriter(dir, new IndexWriterConfig( new StandardAnalyzer()));
+	}
+
+	public static void main(String[] args) throws Exception {
+		 if (args.length != 2) {
+			 throw new IllegalArgumentException("Usage: java " + Indexer.class.getName() + " <index dir> <data dir>");
+		 }
+		 String indexDir = args[0];
+		 String dataDir = args[1];
+		 long start = System.currentTimeMillis();
+		 Indexer indexer = new Indexer(indexDir);
+		 int numIndexed;
+		 try {
+			 numIndexed = indexer.index(dataDir, new TextFilesFilter());
+		 } finally {
+			 indexer.close();
+		 }
+		 long end = System.currentTimeMillis();
+		 System.out.println("Indexing " + numIndexed + " files took " + (end - start) + " milliseconds");
+	 }
+	
+	public void close() throws IOException {
+		 writer.close();
+	 }
+
+	public int index(String dataDir, FileFilter filter) throws Exception {
+		 File[] files = new File(dataDir).listFiles();
+		 for (File f: files)
+			 if (!f.isDirectory() && !f.isHidden() && f.exists() && f.canRead() && (filter == null || filter.accept(f)))
+				 indexFile(f);
+		 return writer.numDocs();
+	 }
+	
+	private static class TextFilesFilter implements FileFilter {
+		 public boolean accept(File path) {
+			 return path.getName().toLowerCase().endsWith(".txt");
+		 }
+	 }
+			 
+	protected Document getDocument(File f) throws Exception {
+		Document doc = new Document();
+		doc.add(new TextField("contents", new FileReader(f)));
+		doc.add(new TextField("filename", f.getName(), Field.Store.YES));
+		doc.add(new TextField("fullpath", f.getCanonicalPath(), Field.Store.YES));
+		return doc;
+	 }
+		
+	private void indexFile(File f) throws Exception {
+		System.out.println("Indexing " + f.getCanonicalPath());
+		writer.addDocument(getDocument(f));
+	}
+}
---
+++ src/chapter1/Searcher.java
@@ -0,0 +1,46 @@
+package chapter1;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+public class Searcher {
+	public static void main(String[] args) throws IllegalArgumentException, IOException, ParseException {
+		 if (args.length != 2) {
+			 throw new IllegalArgumentException("Usage: java " + Searcher.class.getName() + " <index dir> <query>");
+		 }
+		 String indexDir = args[0];
+		 String q = args[1];
+		 search(indexDir, q);
+	 }
+	
+	 public static void search(String indexDir, String q) throws IOException, ParseException {
+		 Directory dir = FSDirectory.open(new File(indexDir).toPath());
+		 DirectoryReader reader = DirectoryReader.open(dir);
+		 IndexSearcher is = new IndexSearcher(reader);
+		 QueryParser parser = new QueryParser("contents", new StandardAnalyzer());
+		 Query query = parser.parse(q);
+		 
+		 long start = System.currentTimeMillis();
+		 TopDocs hits = is.search(query, 10);
+		 long end = System.currentTimeMillis();
+		 System.err.printf("Found %d document(s) (in %d milliseconds) that matched query '%s'", hits.totalHits ,end - start, q);
+		 
+		 for(ScoreDoc scoreDoc : hits.scoreDocs) {
+			 Document doc = is.doc(scoreDoc.doc);
+			 System.out.println(doc.get("fullpath"));
+		 }
+		 reader.close();
+	 }
+}
